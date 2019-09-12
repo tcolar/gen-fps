5ad7d2b16bf194c683002ce22546a6877216f8c1
Thibaut Colar <tcolar@colar.net>
2019-09-11 21:36:02 -0700

    Touch UI

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -7,7 +7,6 @@ public class GameMain : MonoBehaviour
     public void Awake()
     {
         UnityEngine.Random.InitState((int)GameSettings.seed);
-
         TerrainGenerator terrain = new TerrainGenerator();
         terrain.Generate();
     }
